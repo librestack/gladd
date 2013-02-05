a1df7c54a88e3143c4e22ff3ba9a9b42756a1c6c
Brett Sheffield <brett@gladserv.com>
2013-02-05 08:29:37 +0000

    added HTTP_METHOD_NOT_ALLOWED

--- src/http.h
+++ src/http.h
@@ -35,6 +35,7 @@ typedef enum {
         HTTP_UNAUTHORIZED               = 401,
         HTTP_FORBIDDEN                  = 403,
         HTTP_NOT_FOUND                  = 404,
+        HTTP_METHOD_NOT_ALLOWED         = 405,
         HTTP_LENGTH_REQUIRED            = 411,
         HTTP_UNSUPPORTED_MEDIA_TYPE     = 415,
         HTTP_INTERNAL_SERVER_ERROR      = 500,
