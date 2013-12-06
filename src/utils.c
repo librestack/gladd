4c9763a8c7317683f7cbe9fd61d195242a3b23c9
Brett Sheffield <brett@gladserv.com>
2013-12-06 15:45:06 +0000

    add copyright header to utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,3 +1,25 @@
+/*
+ * utils.c - some handy functions that don't fit elsewhere
+ *
+ * this file is part of GLADD
+ *
+ * Copyright (c) 2012, 2013 Brett Sheffield <brett@gladserv.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (see the file COPYING in the distribution).
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
