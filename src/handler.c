5e8da61f4113ebd1ab386047ef7cfea2873cf84c
Brett Sheffield <brett@gladserv.com>
2013-12-09 14:33:16 +0000

    format xml for document upload

--- src/handler.c
+++ src/handler.c
@@ -813,7 +813,8 @@ http_status_code_t response_upload(int sock, url_t *u)
         if (request->uuid) {
                 tmp = strdup(r);
                 free(r);
-                asprintf(&r, "<uuid>%s</uuid>%s", uuid, tmp);
+                asprintf(&r, "<resources><uuid>%s</uuid>\n%s\n</resources>",
+                        uuid, tmp);
                 free(tmp);
         }
         set_headers(&r); /* set any additional headers */
