3f4ab3996b0aad6c11fd6b69eb51be47c9d8d2c9
Brett Sheffield <brett@gladserv.com>
2014-01-03 20:26:09 +0000

    js timeout hacks

--- js/gladd.js
+++ js/gladd.js
@@ -2407,3 +2407,21 @@ function randomString(strlen) {
     }
     return a.join('');
 }
+
+/* setTimeout hacks 
+  source: https://developer.mozilla.org/en-US/docs/Web/API/Window.setTimeout */
+var __nativeST__ = window.setTimeout, __nativeSI__ = window.setInterval;
+ 
+window.setTimeout = function (vCallback, nDelay /*, argumentToPass1, argumentToPass2, etc. */) {
+  var oThis = this, aArgs = Array.prototype.slice.call(arguments, 2);
+  return __nativeST__(vCallback instanceof Function ? function () {
+    vCallback.apply(oThis, aArgs);
+  } : vCallback, nDelay);
+};
+ 
+window.setInterval = function (vCallback, nDelay /*, argumentToPass1, argumentToPass2, etc. */) {
+  var oThis = this, aArgs = Array.prototype.slice.call(arguments, 2);
+  return __nativeSI__(vCallback instanceof Function ? function () {
+    vCallback.apply(oThis, aArgs);
+  } : vCallback, nDelay);
+};
