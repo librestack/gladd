90743001945f1caf280ae5f5835b9c3ba29d83b1
Brett Sheffield <brett@gladserv.com>
2015-04-02 17:05:11 +0100

    increment version to 0.2.11

--- src/main.h
+++ src/main.h
@@ -27,7 +27,7 @@
 #define LOCKFILE_USER ".gladd.pid"
 #define LOCKFILE_ROOT "/var/run/gladd.pid"
 #define PROGRAM "gladd"
-#define VERSION "0.2.10"
+#define VERSION "0.2.11"
 
 #define _unused(x) ((void)x)
 
